cba311ec84fa6f645d073b6035b4831cd0b9d572
jisu <jisu_oh@3ssoft.co.kr>
2026-07-30 13:34:21 +0900

    feat : lint config 개선 - vue lint 규칙 추가

--- lint-server/eslint.config.js
+++ lint-server/eslint.config.js
@@ -1,16 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
+import vue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
 
 export default [
+  ...vue.configs["flat/vue2-essential"],
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx,vue}"],
     languageOptions: {
+      parser: vueParser,
       ecmaVersion: "latest",
       sourceType: "module",
       globals: {
         ...globals.browser,
         ...globals.node,
         ...globals.es2022,
+        ...globals.jquery,
         console: "readonly",
         process: "readonly",
         module: "readonly",
